becef37efa86d6bcb6def7f1eb46c7837c9a5f20
SonicVelikiy <weather_moonlight_1996@mail.ru>
2022-08-24 14:09:28 +0500

    Hotel project

---
+++ src/main/java/uz/pdp/mehmonxona/MehmonxonaApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.mehmonxona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MehmonxonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MehmonxonaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/mehmonxona/MehmonxonaApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.mehmonxona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MehmonxonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
